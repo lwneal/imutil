b608ce5aab95fca5abb2654184c2c7342b1d5514
Lawrence Neal <nealla@lwneal.com>
2018-06-07 12:23:10 -0700

    Correct setup

--- setup.py
+++ setup.py
@@ -3,20 +3,15 @@
 from setuptools import setup
 
 setup(name='imutil',
-        version='0.1.0',
-        description='Swiss army knife for displaying images',
-        author='Larry Neal',
-        author_email='nealla@lwneal.com',
-        packages=[
-            'imutil',
-        ],
-        scripts=['scripts/gnomehat_server',
-                 'scripts/gnomehat_worker',
-                 'scripts/gnomehat_run',
-                 'scripts/gnomehat_cleanup',
-                 'scripts/gnomehat'],
-      install_requires=[
-          "numpy",
-          "Pillow",
-      ],
+    version='0.1.0',
+    description='Swiss army knife for displaying images',
+    author='Larry Neal',
+    author_email='nealla@lwneal.com',
+    packages=[
+        'imutil',
+    ],
+    install_requires=[
+        'numpy',
+        'Pillow',
+    ],
 )
