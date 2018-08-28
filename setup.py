8e7e561e29085536f32bb306d78c198e202f7c87
Lawrence Neal <nealla@lwneal.com>
2018-08-28 13:27:51 -0700

    Increment vesrion

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='imutil',
-    version='0.1.7',
+    version='0.1.8',
     description='Swiss army knife for displaying images',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
