115fed6b96de002b1e2d2ba12192241bc68a9be0
Lawrence Neal <nealla@lwneal.com>
2018-08-29 09:23:43 -0700

    Add test skeleton

---
+++ test_imutil/test_imutil.py
@@ -0,0 +1,10 @@
+import unittest
+import imutil
+
+class TestImutil(unittest.TestCase):
+
+    def test_foo(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
