Fix color normalization issues when resizing

imutil/__init__.py
@@ -25,7 +25,7 @@ def show(
         resize_to=None,
         resize_height=None,
         resize_width=None,
-        normalize_color=True,
+        normalize=True,
         caption=None,
         font_size=16,
         return_pixels=False):
@@ -43,13 +43,14 @@ def show(
     pixels = reshape_ndarray_into_rgb(pixels)
     assert len(pixels.shape) == 3
 
-    # Normalize pixel intensities
-    if normalize_color and pixels.max() > pixels.min():
-        pixels = (pixels - pixels.min()) * 255. / (pixels.max() - pixels.min())
-
     # Resize image to desired shape
     if resize_height or resize_width:
         pixels = resize(pixels, resize_height, resize_width)
+
+    # Normalize pixel intensities
+    if normalize:
+        wtf = normalize_color(pixels)
+        pixels, min_val, max_val = normalize_color(pixels)
 
     # Draw a bounding box onto the image
     if box is not None:
@@ -210,15 +211,25 @@ def combine_images(images, stack_width=None):
 
 # pixels: np.array of shape (height, width, 3)
 def resize(pixels, resize_height, resize_width):
+    pixels, min_val, max_val = normalize_color(pixels)
     current_height, current_width, channels = pixels.shape
     if resize_height is None:
         resize_height = current_height
     if resize_width is None:
         resize_width = current_width
-    img = Image.fromarray((pixels * 255).astype('uint8'))
+    img = Image.fromarray((pixels * 255.).astype('uint8'))
     img = img.resize((resize_width, resize_height))
     pixels = np.array(img).astype(float) / 255.
+    pixels = min_val + (pixels * (max_val - min_val))
     return pixels
+
+
+def normalize_color(pixels):
+    min_val, max_val = pixels.min(), pixels.max()
+    if min_val == max_val:
+        return pixels, min_val, max_val
+    pixels = (pixels - min_val) * 255. / (max_val - min_val)
+    return pixels, min_val, max_val
 
 
 # pixels: np.array of shape (height, width, 3)