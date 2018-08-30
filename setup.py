b9d8dfb2d126c3e576c6bc35d8c7ec30528c9e40
Lawrence Neal <nealla@lwneal.com>
2018-08-29 21:09:55 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='imutil',
-    version='0.1.11',
+    version='0.1.12',
     description='Swiss army knife for displaying images',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
