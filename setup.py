aa865eeb203e5913434aff6f1ba0f429d8a642f6
Lawrence Neal <nealla@lwneal.com>
2018-10-09 13:31:11 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='imutil',
-    version='0.1.15',
+    version='0.1.16',
     description='Swiss army knife for displaying images',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
