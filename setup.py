23d36e67c69c18f76e63761234eab874b550d39c
Lawrence Neal <nealla@lwneal.com>
2018-11-01 12:49:56 -0700

    Add long_description

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(name='imutil',
     version='0.1.17',
     description='Swiss army knife for displaying images',
+    long_description='Swiss army knife for converting and displaying numpy arrays, PyTorch tensors, and other image-like objects',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
     url="https://github.com/lwneal/imutil",
