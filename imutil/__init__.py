3563ee46a166818336059af0fb77f6ae591c5d36
Lawrence Neal <nealla@lwneal.com>
2018-11-27 21:58:54 -0800

    Fix invalid loglevel
    "panic", not "-panic"

--- imutil/__init__.py
+++ imutil/__init__.py
@@ -313,7 +313,7 @@ def display_image_on_screen(filename):
 def encode_video(video_filename, loopy=False):
     output_filename = video_filename.replace('mjpeg', 'mp4')
     print('Encoding MJPEG video {}'.format(video_filename))
-    cmd = ['ffmpeg', '-hide_banner', '-nostdin', '-loglevel', '-panic', '-y', '-i', video_filename]
+    cmd = ['ffmpeg', '-hide_banner', '-nostdin', '-loglevel', 'panic', '-y', '-i', video_filename]
     if loopy:
         cmd += ['-filter_complex', '"[0]reverse[r];[0][r]concat"']
     cmd += [output_filename]
