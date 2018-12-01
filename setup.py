c61829842e3e3242037362efe9500a51f38201eb
Lawrence Neal <nealla@lwneal.com>
2018-12-01 00:33:55 -0800

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='imutil',
-    version='0.1.20',
+    version='0.1.21',
     description='Swiss army knife for displaying images',
     long_description='Swiss army knife for converting and displaying numpy arrays, PyTorch tensors, and other image-like objects',
     author='Larry Neal',
